2ef9103e885e072c3f732edfd84b0c6bfc366e0a
Raj Shekhar <sanjayrajs123@gmail.com>
2024-09-18 21:07:31 +0530

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,7 +11,7 @@ for(let i=1;i<=4;i++){
     
 }
 
-console.log(i);  // this will give error because i can only be used in the block
+// console.log(i);  // this will give error because i can only be used in the block
 
 
 
@@ -24,3 +24,49 @@ function Calsum(a,b){
 Calsum(3,4) //o/p --> 7
 
 console.log(sum); // o/p is  34
+
+//*********NESTED SCOPE */
+
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+        
+    }
+
+    // console.log(website)
+    two()
+}
+
+one()
+
+if(true){
+    const username="rambo35@gmail.com"
+    if(username==="rambo35@gmail.com"){
+        const website="outlook";
+        console.log(username+ website);
+    }
+    // console.log(website);  // This line gives error because the website is accessed outside the block.
+    
+}
+
+// console.log(username)      // This code also gives err because we are trying to access the username outside the block.
+
+//********************HOISTING CONCEPT */
+
+console.log(addone(5));      // here in function we can also call it above the function
+
+function addone(num){
+    return num+1
+}
+
+
+// console.log(addtwo(5));  // Here it gives error, because we have declared it in a variable, and before declaring variable we cannot call it, this is called as hoisting
+
+
+const addtwo=function(num){
+    return num+1
+}
+
