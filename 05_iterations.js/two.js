af2fe10835823e1584c28546efed7546e0bff096
Raj Shekhar <sanjayrajs123@gmail.com>
2024-09-22 15:28:36 +0530

    while and do while

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,23 @@
+//While
+
+let index=0
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index=index+2
+}
+
+
+let supes=["Iron man","Thor","Captain America"]
+let arr=0
+while(arr<supes.length){
+    console.log(`value is ${supes[arr]}`);
+    arr=arr+1;
+}
+
+//do while loop
+
+let score=1
+do {
+    console.log(`score is ${score}`)
+    score++;
+} while (score<=10);
